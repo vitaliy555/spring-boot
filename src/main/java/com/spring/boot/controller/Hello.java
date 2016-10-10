afd13becee7cd38137d77cabbb868c8e28aa3858
Vitalii Chechel <vitalii_chechel@epam.com>
2016-10-10 22:18:30 +0300

    fix controller and add static cintext

--- src/main/java/com/spring/boot/controller/Hello.java
+++ src/main/java/com/spring/boot/controller/Hello.java
@@ -2,13 +2,13 @@ package com.spring.boot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/")
+@RestController
 public class Hello {
 
     @RequestMapping("hello")
     public String sayHello(){
-        return "Hello";
+        return "Hello world";
     }
 }
