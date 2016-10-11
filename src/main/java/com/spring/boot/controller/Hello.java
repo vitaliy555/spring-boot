3202d9083943d6402ae911ac6aa9b12f38f803d0
Vitalii Chechel <vitalii_chechel@epam.com>
2016-10-11 16:08:10 +0300

    path

--- src/main/java/com/spring/boot/controller/Hello.java
+++ src/main/java/com/spring/boot/controller/Hello.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class Hello {
 
-//    @RequestMapping("hello")
+    @RequestMapping("hello")
     public String sayHello(){
         return "Hello world";
     }
