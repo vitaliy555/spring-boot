64415407c184d556f956437f078e2101d9986d9e
Vitalii Chechel <vitalii_chechel@epam.com>
2016-10-14 13:22:34 +0300

    hello jsp

--- src/main/java/com/spring/boot/Config.java
+++ src/main/java/com/spring/boot/Config.java
@@ -1,33 +1,25 @@
 package com.spring.boot;
 
-import com.lowagie.text.Table;
-import com.lowagie.text.pdf.PdfWriter;
-import com.spring.boot.view_resolvers.PdfView;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.XmlViewResolver;
-import org.springframework.web.servlet.view.document.AbstractPdfView;
-import com.lowagie.text.Document;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 
 @Configuration
 public class Config {
+//
+//    @Bean
+//    public XmlViewResolver xmlViewResolver() {
+//        XmlViewResolver xmlViewResolver = new XmlViewResolver();
+//        xmlViewResolver.setOrder(1);
+//        return xmlViewResolver;
+//    }
 
     @Bean
-    public XmlViewResolver xmlViewResolver(){
-        XmlViewResolver xmlViewResolver = new XmlViewResolver();
-        xmlViewResolver.setOrder(1);
-        return xmlViewResolver;
-    }
-
-    @Bean
-    public InternalResourceViewResolver internalResourceViewResolver(){
+    public InternalResourceViewResolver internalResourceViewResolver() {
         InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-        internalResourceViewResolver.setOrder(2);
+        internalResourceViewResolver.setPrefix("/WEB-INF/pages/");
+        internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
     }
 
--- src/main/java/com/spring/boot/controller/Hello.java
+++ src/main/java/com/spring/boot/controller/Hello.java
@@ -1,14 +1,23 @@
 package com.spring.boot.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.constraints.NotNull;
 
 @RestController
 public class Hello {
 
-    @RequestMapping("hello")
-    public String sayHello(){
-        return "Hello world";
+    @RequestMapping("helloJSP")
+    public ModelAndView helloJSP() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("HelloPage");
+        return modelAndView;
     }
+    @RequestMapping("hello")
+    public String sayHello() {
+        return "Plain text hello";
+    }
+
 }
